b32f8d094eb8c2671e0e498f48beb1fb4f64ae76
tnubeo1111 <pelu0377583903@gmail.com>
2024-06-01 22:09:55 +0700

    edit file k8s

--- build-k8s/auto-k8s.sh
+++ build-k8s/auto-k8s.sh
@@ -2,6 +2,6 @@
 
 # Login  root
 
-sudo bash ./in-docker-k8s.sh
+sudo bash ./build-k8s/n-docker-k8s.sh
 
-sudo bash ./build-k8s-cluster-all-in-one.sh
+sudo bash ./build-k8s/build-k8s-cluster-all-in-one.sh
--- build-k8s/build-k8s-cluster-all-in-one.sh
+++ build-k8s/build-k8s-cluster-all-in-one.sh
@@ -57,6 +57,11 @@ kubectl create -f custom-resources.yaml
 
 # Setup  Storage Class
 
-echo "https://github.com/kubesphere/ks-devops/discussions/30"
+kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
+kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+
+# Install Ingress
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.3.0/deploy/static/provider/cloud/deploy.yaml
+
 
 echo "Done !!! --> https://www.cherryservers.com/blog/install-kubernetes-on-ubuntu"
